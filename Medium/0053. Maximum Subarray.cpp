23806fd8a93b1e3c27282029b3fbffebf470323f
ADITYA PANDEY <142161926+adityaap55@users.noreply.github.com>
2023-12-02 23:59:07 +0530

    Create 0053. Maximum Subarray.cpp

---
+++ Medium/0053. Maximum Subarray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int result=numeric_limits<int>::min();
+        int curr=numeric_limits<int>::min();
+        for(const auto &x : nums)
+        {
+            curr=(curr==numeric_limits<int>::min())? x : max(curr+x,x);
+            result=max(result,curr);
+        }
+        return result;
+    }
+};
